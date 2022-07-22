878d0eebc299b353d0fd0d3a43cf259067016add
Anas <anas.jasim9911@gmail.com>
2022-07-22 19:21:13 +0300

    improve pdf resolution by using devicePixelRatio

--- ogn-frontend/src/lib/modules/pdf/views/PdfPage.svelte
+++ ogn-frontend/src/lib/modules/pdf/views/PdfPage.svelte
@@ -35,13 +35,22 @@
 	async function renderPage(page, viewport) {
 		if (pageRenderTask?._internalRenderTask.running) pageRenderTask.cancel()
 		const context = canvas.getContext('2d')
-		canvas.height = viewport.height
-		canvas.width = viewport.width
+
+		const outputScale = window.devicePixelRatio || 1
+
+		canvas.width = Math.floor(viewport.width * outputScale)
+		canvas.height = Math.floor(viewport.height * outputScale)
+		canvas.style.width = Math.floor(viewport.width) + 'px'
+		canvas.style.height = Math.floor(viewport.height) + 'px'
+
+		var transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null
 
 		const renderContext = {
 			canvasContext: context,
 			viewport: viewport,
+			transform,
 		}
+
 		pageRenderTask = page.render(renderContext)
 	}
 	async function renderText(page, viewport) {
