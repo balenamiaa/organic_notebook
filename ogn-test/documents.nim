3e02e772d90e6fb2b0e4348bff6eca33fcf2e80a
balenamiaa <barena.ekusuimii@gmail.com>
2022-07-29 01:25:07 +0300

    test fixes

--- ogn-test/documents.nim
+++ ogn-test/documents.nim
@@ -4,7 +4,7 @@ const DOCUMENT_TITLE = "Anas's Grimoire of Red Magic"
 
 let createResp = makeReq("/documents", "post", "status,json", files = block:
   var files = newMultipartData()
-  files.addFiles({"test_document": "./Anas's Grimoire of Red Magic.pdf"})
+  files.addFiles({"test_document": "ogn-test/Anas's Grimoire of Red Magic.pdf"})
 )
 
 doAssert createResp.status == Http200
@@ -37,9 +37,9 @@ let deleteIdeaResp = makeReq("/documents/" & createdDocument["id"].str,
     "delete", "status,string")
 
 doAssert deleteIdeaResp.status == Http200
-doAssert deleteIdeaResp.val.parseInt == 1 # num of deleted ideas
+doAssert deleteIdeaResp.val.parseInt == 1 # num of deleted documents
 
 let attemptGetResp = makeReq("/ideas/" & createdDocument["id"].str, "get",
-    "status") # attempt to get deleted idea
+    "status") # attempt to get deleted document
 
 doAssert not(attemptGetResp == Http200)
